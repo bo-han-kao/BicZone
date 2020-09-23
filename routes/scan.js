d060fdbaae50967ee14a356112fac87e42b4800a
bo-han-kao <54506408+bo-han-kao@users.noreply.github.com>
2020-09-23 18:08:21 +0800

    可以讀取url上的data 看能不能及時顯示資料

--- routes/scan.js
+++ routes/scan.js
@@ -1,28 +1,40 @@
 var express = require('express');
 var router = express.Router();
-
+const fs = require('fs');
+const axios = require('axios').default;
 // middleware程式碼，這樣才能抓倒頁面資料
 router.use(express.urlencoded({
-    extended: false
-  }));
-  
-  
-  router.use(express.json());
+  extended: false
+}));
+
+
+router.use(express.json());
+// router.get('/', function(req, res, next) {
+//     var scandata = require('../scandata.json');
+//     var device=scandata.payload.devices
+//     console.log(device);
+//     res.render('scan',{
+//       devices:device,
+//     });
+//   });
 
 
 
-
-router.get('/', function(req, res, next) {
-    var scandata = require('../scandata.json');
-    var device=scandata.payload.devices
-    console.log(device);
-    res.render('scan',{
-      devices:device,
-    });
+router.get('/', async (req, res)=>{
+  const response = await (await axios.get('http://127.0.0.1:5000/v1/mesh/scan')).data;
+  var devices=response.payload.devices
+  console.log(devices.length);
+  res.render('scan', {
+    devices: devices,
   });
+});
 
-
-
+router.get('/get-data', async (req, res)=>{
+  const response = await (await axios.get('http://127.0.0.1:5000/v1/mesh/scan')).data;
+  var devices=response.payload.devices
+  console.log(devices.length);
+  res.send(devices);
+});
 
 
 module.exports = router;
