31b4f5bf62b9cb9ee971637a11d6daf5d24547f2
bo-han-kao <54506408+bo-han-kao@users.noreply.github.com>
2020-09-25 17:59:47 +0800

    資料換成ajax來渲染 不用ejs

---
+++ public/js/scanView.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function () {
+    setInterval(function () {
+        $.ajax({
+            // "url": "http://localhost:8000/mesh/scan/get-data",
+            "url": "http://localhost:5000/v1/mesh/scan",
+            "success": function (data) {
+                console.log(data.payload)
+                let orders = data.payload.devices
+                // console.log(orders[0])
+                // console.log(orders.length);
+                let str = "";
+                if (orders.length == orders.length) {
+                    for (let i = 0; i < orders.length; i++) {
+                        // console.log(orders[i].UUID);
+                        // console.log(orders[i].name);
+                        // str+='<p>'+orders[i].name+'</p>'
+                        str += '<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">'
+                        str += '<div class="d-flex ">'
+                        str += ' <div>'
+                        str += '<i class="far fa-lightbulb fa-lg pt-3 pr-3"></i>'
+                        str += '</div>'
+                        str += '<div class="d-flex flex-column">'
+                        str += '<span class="item-text">' + orders[i].name + '</span>'
+                        str += ' <span class="item-text">' + orders[i].UUID + '</span>'
+                        str += '</div>'
+                        str += ' </div>'
+                        str += ' <div>'
+                        str += '<button type="button" class="btn btn-outline-success mr-2" value="alert" >link</button>'
+                        str += '<button class=" btn btn-outline-primary btn-sm"><i class="fas fa-wifi"></i></button>'
+                        str += ' </div>'
+                        str += ' </li>'
+                    }
+                }
+                $('#menu').html(str);
+            },
+            "error": function (error) {
+                console.log(error);
+            }
+        })
+
+
+    }, 10000);
+
+})
+
--- routes/scan.js
+++ routes/scan.js
@@ -20,20 +20,33 @@ router.use(express.json());
 
 
 
-router.get('/', async (req, res)=>{
-  const response = await (await axios.get('http://127.0.0.1:5000/v1/mesh/scan')).data;
-  var devices=response.payload.devices
-  console.log(devices.length);
-  res.render('scan', {
-    devices: devices,
+// router.get('/', async (req, res)=>{
+//   const response = await (await axios.get('http://127.0.0.1:5000/v1/mesh/scan')).data;
+//   var devices=response.payload.devices
+//   console.log(devices.length);
+//   res.render('scan', {
+//     devices: devices,
+//   });
+// });
+
+
+
+router.get('/', async (req, res) => {
+  // const response = await (await axios.get('http://localhost:8000/mesh/scan/get-data')).data;
+  // const response = await (await axios.get('http://localhost:5000/v1/mesh/scan')).data;
+  res.render('scan',{
+    // devices: response.devices,
   });
 });
 
-router.get('/get-data', async (req, res)=>{
-  const response = await (await axios.get('http://127.0.0.1:5000/v1/mesh/scan')).data;
-  var devices=response.payload.devices
-  console.log(devices.length);
-  res.send(devices);
+router.get('/get-data', (req, res) => {
+  try {
+    const scandata = JSON.parse(fs.readFileSync('./scandata.json', { encoding: 'utf-8' }));
+    res.status(200).send({ devices: scandata.payload.devices });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error }).end();
+  }
 });
 
 
