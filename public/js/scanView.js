be3cf60e74932d4e9a592af9fbbb7154e7903adb
bo-han-kao <54506408+bo-han-kao@users.noreply.github.com>
2020-10-05 11:40:45 +0800

    修正button多重綁定 跟 定時器第一次不延遲

--- public/js/scanView.js
+++ public/js/scanView.js
@@ -1,6 +1,6 @@
-
 $(document).ready(function () {
-    setInterval(function () {
+
+    var countInterval=setInterval(function interval() {
         $.ajax({
             // 假資料測試用
             // "url": "http://localhost:8000/mesh/scan/get-data",
@@ -8,40 +8,43 @@ $(document).ready(function () {
             "success": function (data) {
                 console.log(data.payload)
                 let orders = data.payload.devices
-                // console.log(orders[0])
-                // console.log(orders.length);
                 let str = "";
-                if (orders.length == orders.length) {
-                    for (let i = 0; i < orders.length; i++) {
-                        // console.log(orders[i].UUID);
-                        // console.log(orders[i].name);
-                        // str+='<p>'+orders[i].name+'</p>'
-                        str += '<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">'
-                        str += '<div class="d-flex ">'
-                        str += ' <div>'
-                        str += '<i class="far fa-lightbulb fa-lg pt-3 pr-3"></i>'
-                        str += '</div>'
-                        str += '<div class="d-flex flex-column">'
-                        str += '<span class="item-text">' + orders[i].name + '</span>'
-                        str += ' <span class="item-text">' + orders[i].UUID + '</span>'
-                        str += '</div>'
-                        str += ' </div>'
-                        str += ' <div>'
-                        str += '<button type="button" class="btn btn-outline-success mr-2" value="alert" >link</button>'
-                        str += '<button class=" btn btn-outline-primary btn-sm"><i class="fas fa-wifi"></i></button>'
-                        str += ' </div>'
-                        str += ' </li>'
-                    }
+
+                for (let i = 0; i < orders.length; i++) {
+                    // console.log(orders[i].UUID);
+                    // console.log(orders[i].name);
+                    // str+='<p>'+orders[i].name+'</p>'
+                    str += '<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">'
+                    str += '<div class="d-flex ">'
+                    str += ' <div>'
+                    str += '<i class="far fa-lightbulb fa-lg pt-3 pr-3"></i>'
+                    str += '</div>'
+                    str += '<div class="d-flex flex-column">'
+                    str += '<span class="item-text">' + orders[i].name + '</span>'
+                    str += ' <span class="item-text">' + orders[i].UUID + '</span>'
+                    str += '</div>'
+                    str += ' </div>'
+                    str += ' <div>'
+                    str += '<button type="button" class="btn btn-outline-success mr-2" value="alert" data-id=' + orders[i].UUID + '>link</button>'
+                    str += '<button class=" btn btn-outline-primary btn-sm"><i class="fas fa-wifi"></i></button>'
+                    str += ' </div>'
+                    str += ' </li>'
                 }
                 $('#menu').html(str);
+
+                // 重新綁定才不會受setInterval影響
+                $(document).off("click").on("click", function (e) {
+                    let _id = e.target.getAttribute("data-id");
+                    console.log(_id);
+                });
             },
             "error": function (error) {
                 console.log(error);
             }
         })
-
-
-    }, 10000);
+    
+    return interval;  
+    }(), 10000);
 
 })
 
