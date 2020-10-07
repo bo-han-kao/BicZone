90e9d3d1c2b4f127156c1e014b7ff3f886f7d38d
bo-han-kao <54506408+bo-han-kao@users.noreply.github.com>
2020-10-07 14:27:20 +0800

    device頁面雛形

--- public/js/device.js
+++ public/js/device.js
@@ -1,9 +1,33 @@
-$(function(){
-    var $li = $('ul.tab-title li');
-        $($li. eq(0) .addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
-    
-        $li.click(function(){
-            $($(this).find('a'). attr ('href')).show().siblings ('.tab-inner').hide();
-            $(this).addClass('active'). siblings ('.active').removeClass('active');
-        });
-    });
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+	// -------------------tabs------------------
+	$(function () {
+		var $li = $('ul.tab-title li');
+		$($li.eq(0).addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
+
+		$li.click(function () {
+			$($(this).find('a').attr('href')).show().siblings('.tab-inner').hide();
+			$(this).addClass('active').siblings('.active').removeClass('active');
+		});
+	});
+	// -------------------tabs------------------
+	// -------------------toggle------------------
+	$("#provide-muffins").click(function () {
+		checkis = $(this).is(":checked");
+		// var checkis = $(this).val();
+		console.log(checkis);
+	});
+	// console.log(checkis);
+	// -------------------toggle------------------
+
+
+
+})
+
